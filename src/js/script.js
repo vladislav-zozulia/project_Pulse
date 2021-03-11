730e8d882d7a2145d7b333998507bb17799ae7e6
Vladislav <vladislav_zozulia@ukr.net>
2021-03-11 20:15:47 +0300

    Try to make slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.caurosel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/icon_preva.svg" alt="Left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/icon_nexta.svg" alt="Right"></button>'
+      });
+  });
+              
